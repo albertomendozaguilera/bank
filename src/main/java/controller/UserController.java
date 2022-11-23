ef4544d3365ca3fd68b0ad0614b89accfa524f8c
albertomendozaguilera <albertomendozaguilera@gmail.com>
2022-11-23 16:42:41 +0100

    add cors

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -3,16 +3,19 @@ package controller;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import model.User;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.PUT, RequestMethod.POST})
 public class UserController {
 	
 	private DaoService service;
